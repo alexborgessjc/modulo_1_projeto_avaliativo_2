5a5dde96ea65f08fa0925c6ec2377274f18f7c9b
Alex Borges <alexborgessjc@hotmail.com>
2022-01-31 18:30:22 -0300

    Envio 8 do projeto - Grafico funcionando sem os dados

--- src/components/Grafico/index.js
+++ src/components/Grafico/index.js
@@ -1,52 +1,61 @@
 import React from 'react'
-/*
-import React from 'react';
+
 import { Line } from 'react-chartjs-2';
- const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+export default function Chart({ labels, dataPlot}){
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    scales: {
+      x:{
+        grid:{
+          display: false
+        }
+      },
+      y:{
+        position:'left'
+      }
     },
-    title: {
-      display: true,
-      text: 'Meu novo titulo',
+    plugins:{
+      legend:{
+        display: true,
+      },
+      title:{
+        display: false,
+      },
     },
-  },
 };
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agosto'];
-export const data = {
-  labels,
-  datasets: [
+
+const data = {
+  labels:['Janeiro', 'Fevereiro','Março', 'Abril', 'Maio', 'Junho'],
+  datasets:[
     {
-      label: 'Quantidade de vacinados',
-      data: [1000, 2000, 3000, 1000, 500, 100, 2000, 400],
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Quantidade de vacinados',
-      data: [100, 200, 12, 100, 12, 100, 200, 40],
-      borderColor: 'red',
-      backgroundColor: 'red',
+      label: 'Geração',
+      data: [10,20,30], //tratar o dado
+      borderColor: '#2196f3',
+      backgroundColor: '#2196f3',
     },
   ],
 };
-*/
-
-function Grafico() {
-  return (
-    /*
-      <>
-        <Line options={options} data={data} />
-      </>
-    */
-    <div>      
-        
-        O gráfico não funciona, tentei diversas vezes e nao sei pq nao funciona.<br/>
-        Deixei o código comentado na página.
-     </div> 
-  )
+  return <Line options={options} data={data}/>;
 }
-
-export default Grafico
